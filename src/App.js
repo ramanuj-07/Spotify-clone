9415ed381fa84be0ad6854e0336ea5b07ea3ad32
ramanuj-07 <ramanujchaurasia09@gmail.com>
2024-06-30 10:50:57 +0530

    Update the App.js along with Navigation Bar

--- src/App.js
+++ src/App.js
@@ -1,25 +1,69 @@
-import logo from './logo.svg';
 import './App.css';
+import styled from 'styled-components';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <NavigationBar>
+        <ul>
+          <li className="brand">
+            <img src="Spotify_icon.svg.webp" alt="Logo" /> Spotify
+          </li>
+          <li className="icons">Home</li>
+          <li className="icons">About</li>
+          <li className="searchBar">
+            <input type="text" id="searchInput" placeholder="Search song" />
+            <button onClick="">
+              <span className="material-symbols-outlined">
+                search
+              </span>
+            </button>
+          </li>
+        </ul>
+      </NavigationBar>
+      <Container>
+
+
+      </Container>
     </div>
   );
 }
 
 export default App;
+
+const Container = styled.div`
+    background-color: black;
+    color: white;
+    display: flex;
+    margin: 23px auto;
+    width: 70%;
+    border-radius: 12px;
+    padding: 20px;
+    background-image: url('depositphotos_21380509-stock-illustration-music-equaliser-1.jpeg');
+
+`
+
+const NavigationBar = styled.div`
+  font-family: 'Montserrat', sans-serif;
+
+  input {
+    padding: 6px;
+    width: 200px;
+    margin-left: 650px;
+  }
+
+  ul {
+    display: flex;
+    list-style-type: none;
+    height: 60px;
+    background-color: black;
+  }
+
+  li {
+    padding: 0 15px;
+    color: white;
+    display: flex;
+    align-items: center;
+    /* cursor: pointer; */
+  }
+`;
